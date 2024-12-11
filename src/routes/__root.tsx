30d9dc4634abe43268eda779befe985b874ba333
Gabriel5934 <gabriel.andrade@outlook.com.br>
2024-12-11 14:09:20 -0300

    feat: update desktop layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -57,45 +57,47 @@ export const Route = createRootRoute({
 
     const years = new Date().getFullYear() - GRADUATION_YEAR;
 
-    const carouselResponsive = {
+    const carouselResponsive = (
+      iDesktop: number,
+      iTablet: number,
+      iMobile: number
+    ) => ({
       desktop: {
         breakpoint: {
-          max: 3000,
+          max: 9999,
           min: 1024,
         },
-        items: 5,
+        items: iDesktop,
         partialVisibilityGutter: 40,
+      },
+
+      tablet: {
+        breakpoint: {
+          max: 1023,
+          min: 768,
+        },
+        items: iTablet,
+        partialVisibilityGutter: 30,
       },
       mobile: {
         breakpoint: {
-          max: 464,
+          max: 767,
           min: 0,
         },
-        items: 2,
+        items: iMobile,
         partialVisibilityGutter: 30,
       },
-      tablet: {
-        breakpoint: {
-          max: 1024,
-          min: 464,
-        },
-        items: 3,
-        partialVisibilityGutter: 30,
-      },
-    };
+    });
 
     return (
-      <div
-      // style={{
-      //   maxWidth: "500px",
-      // }}
-      >
+      <div className="w-screen flex flex-col items-center">
         <LeadDialog
           id={DIALOG_CATALOG_ID}
           title="receba o catálogo"
           subtitle="Juw Arquitetura"
         />
-        <div className="bg-green flex justify-between items-center lg:px-8 lg:py-4">
+        {/* Navbar */}
+        <div className="bg-green flex justify-between items-center lg:px-8 lg:py-4 w-full">
           <svg
             viewBox="0 0 70 70"
             fill="none"
@@ -137,7 +139,8 @@ export const Route = createRootRoute({
             </svg>
           </a>
         </div>
-        <div className="flex flex-col justify-center text-center gap-8 bg-darkBase h-[350px] lg:h-[700px]">
+        {/* Hero */}
+        <div className="flex flex-col justify-center text-center gap-8 bg-darkBase h-[350px] lg:h-[700px] w-full">
           <img
             src={Hero}
             alt=""
@@ -191,67 +194,51 @@ export const Route = createRootRoute({
             </a>
           </div>
         </div>
-        <div className="flex justify-center w-full pt-8 lg:pt-16 px-4">
-          <div className="flex items-end gap-4 lg:gap-8">
-            <div className="bg-darkBase bg-no-repeat bg-cover flex flex-col gap-2 relative overflow-clip mb-8 lg:mb-16">
-              <div className="flex flex-col">
-                <span className="text-xl font-medium lg:text-4xl">
-                  Catálogo Juw Arquitetura
-                </span>
-                <span className="text-base lg:text-2xl">
-                  meus projetos para você se inspirar
-                </span>
-              </div>
-              <div>
-                <button
-                  className="btn btn-primary uppercase lg:btn-lg"
-                  onClick={toggleDialogCatalog}
-                >
-                  eu quero!
-                </button>
-              </div>
-            </div>
-            <img src={MiniJu} alt="" className="h-[200px] lg:h-[300px]" />
-          </div>
-        </div>
-        <div
-          className="bg-darkBase"
-          style={{
-            maxWidth: "100vw",
-            overflowX: "hidden",
-          }}
-        >
-          <div className="flex flex-col lg:gap-16  bg-lightBase">
-            <div className="lg:hidden text-primary px-8 rounded-t-3xl lg:rounded-none flex flex-col pt-8 lg:px-32 lg:pt-16 lg:text-center">
-              <span className="font-medium text-lg lg:text-4xl">
-                Deixando cada cômodo e ambiente
+        {/* Catalog */}
+        <div className="flex items-start gap-4 lg:gap-8 mt-8 px-4">
+          <div className="bg-darkBase bg-no-repeat bg-cover flex flex-col gap-2 relative overflow-clip mt-8 lg:mb-16">
+            <div className="flex flex-col">
+              <span className="text-xl font-medium lg:text-4xl">
+                Catálogo Juw Arquitetura
               </span>
-              <span className="font-bold text-2xl underline lg:text-4xl ">
-                do seu jeito
+              <span className="text-base lg:text-2xl">
+                meus projetos para você se inspirar
               </span>
             </div>
-            <div className="carouselContainer">
-              <span>Banheiros e Lavabos</span>
-              <Carousel
-                className="carousel w-screen"
-                centerMode={true}
-                infinite
-                responsive={carouselResponsive}
-                swipeable
-                minimumTouchDrag={0}
+            <div>
+              <button
+                className="btn btn-primary uppercase lg:btn-lg"
+                onClick={toggleDialogCatalog}
               >
-                <img alt="" src={bath1} />
-                <img alt="" src={bath2} />
-                <img alt="" src={bath3} />
-                <img alt="" src={bath4} />
-                <img alt="" src={bath5} />
-                <img alt="" src={bath6} />
-                <img alt="" src={bath7} />
-                <img alt="" src={bath8} />
-                <img alt="" src={bath9} />
-                <img alt="" src={bath10} />
-              </Carousel>
+                eu quero!
+              </button>
             </div>
+          </div>
+          <img src={MiniJu} alt="" className="h-[200px] lg:h-[300px]" />
+        </div>
+
+        <div className="max-w-[1000px] bg-lightBase rounded-xl py-8  w-full">
+          <div className="carouselContainer">
+            <span>Banheiros e Lavabos</span>
+            <Carousel
+              className="carousel"
+              centerMode={true}
+              infinite
+              responsive={carouselResponsive(3, 6, 2)}
+              swipeable
+              minimumTouchDrag={0}
+            >
+              <img alt="" src={bath1} />
+              <img alt="" src={bath2} />
+              <img alt="" src={bath3} />
+              <img alt="" src={bath4} />
+              <img alt="" src={bath5} />
+              <img alt="" src={bath6} />
+              <img alt="" src={bath7} />
+              <img alt="" src={bath8} />
+              <img alt="" src={bath9} />
+              <img alt="" src={bath10} />
+            </Carousel>
           </div>
           <div className="flex flex-col justify-center py-16 bg-cover bg-center gap-2 h-[200px] lg:h-[400px]">
             <span className="text-center z-10 text-white font-medium text-lg lg:text-2xl">
@@ -271,7 +258,7 @@ export const Route = createRootRoute({
             <img
               src={HappyCouple}
               alt=""
-              className="absolute object-cover w-full z-0 h-[200px] lg:h-[400px] object-top lg:object-left-top"
+              className="absolute object-cover w-screen z-0 h-[200px] lg:h-[400px] object-top lg:object-left-top max-w-[1000px]"
               style={{
                 filter: "brightness(0.3)",
               }}
@@ -280,10 +267,10 @@ export const Route = createRootRoute({
           <div className="carouselContainer">
             <span>Cozinhas e Áreas Gourmet</span>
             <Carousel
-              className="carousel w-screen"
+              className="carousel"
               centerMode={true}
               infinite
-              responsive={carouselResponsive}
+              responsive={carouselResponsive(3, 6, 2)}
               swipeable
               minimumTouchDrag={0}
             >
@@ -347,10 +334,10 @@ export const Route = createRootRoute({
           <div className="carouselContainer">
             <span>Quartos e Closets</span>
             <Carousel
-              className="carousel w-screen"
+              className="carousel"
               centerMode={true}
               infinite
-              responsive={carouselResponsive}
+              responsive={carouselResponsive(3, 6, 2)}
               swipeable
               minimumTouchDrag={0}
             >
@@ -364,10 +351,10 @@ export const Route = createRootRoute({
           <div className="carouselContainer">
             <span>Áreas de Convivência</span>
             <Carousel
-              className="carousel w-screen"
+              className="carousel"
               centerMode={true}
               infinite
-              responsive={carouselResponsive}
+              responsive={carouselResponsive(3, 6, 2)}
               swipeable
               minimumTouchDrag={0}
             >
@@ -380,6 +367,7 @@ export const Route = createRootRoute({
             </Carousel>
           </div>
         </div>
+
         <div className="bg-darkBase text-center flex flex-col p-4 gap-4 lg:p-16">
           <div className="flex flex-col">
             <span className="font-medium lg:text-2xl">Entre em contato</span>
@@ -424,51 +412,66 @@ export const Route = createRootRoute({
             </a>
           </div>
         </div>
-        <div className="bg-green px-4 py-16 flex flex-col gap-8">
-          <div className="flex justify-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`w-[20px] lg:w-[30px]`}
-              viewBox="0 0 21 20"
-              fill="none"
+        <div className="bg-green px-4 lg:px-16 py-16 flex flex-col gap-8 w-full">
+          <div className="flex justify-center gap-4">
+            <a
+              href="https://wa.me/5512991820133"
+              target="_blank"
+              rel="noopener"
             >
-              <g id="iconmonstr-whatsapp-1 1" clip-path="url(#clip0_92_71)">
-                <path
-                  id="Vector"
-                  d="M0.547485 20L1.95332 14.8642C1.08582 13.3608 0.629985 11.6567 0.630819 9.90917C0.633319 4.44583 5.07915 0 10.5417 0C13.1925 0.000833333 15.6808 1.03333 17.5525 2.90667C19.4233 4.78 20.4533 7.27 20.4525 9.91833C20.45 15.3825 16.0042 19.8283 10.5417 19.8283C8.88332 19.8275 7.24915 19.4117 5.80165 18.6217L0.547485 20ZM6.04498 16.8275C7.44165 17.6567 8.77498 18.1533 10.5383 18.1542C15.0783 18.1542 18.7767 14.4592 18.7792 9.91667C18.7808 5.365 15.1 1.675 10.545 1.67333C6.00165 1.67333 2.30582 5.36833 2.30415 9.91C2.30332 11.7642 2.84665 13.1525 3.75915 14.605L2.92665 17.645L6.04498 16.8275ZM15.5342 12.2742C15.4725 12.1708 15.3075 12.1092 15.0592 11.985C14.8117 11.8608 13.5942 11.2617 13.3667 11.1792C13.14 11.0967 12.975 11.055 12.8092 11.3033C12.6442 11.5508 12.1692 12.1092 12.025 12.2742C11.8808 12.4392 11.7358 12.46 11.4883 12.3358C11.2408 12.2117 10.4425 11.9508 9.49665 11.1067C8.76082 10.45 8.26332 9.63917 8.11915 9.39083C7.97498 9.14333 8.10415 9.00917 8.22748 8.88583C8.33915 8.775 8.47499 8.59667 8.59915 8.45167C8.72499 8.30833 8.76582 8.205 8.84915 8.03917C8.93165 7.87417 8.89082 7.72917 8.82832 7.605C8.76582 7.48167 8.27082 6.2625 8.06499 5.76667C7.86332 5.28417 7.65915 5.34917 7.50749 5.34167L7.03249 5.33333C6.86749 5.33333 6.59915 5.395 6.37249 5.64333C6.14582 5.89167 5.50582 6.49 5.50582 7.70917C5.50582 8.92833 6.39332 10.1058 6.51665 10.2708C6.64082 10.4358 8.26248 12.9375 10.7467 14.01C11.3375 14.265 11.7992 14.4175 12.1583 14.5317C12.7517 14.72 13.2917 14.6933 13.7183 14.63C14.1942 14.5592 15.1833 14.0308 15.39 13.4525C15.5967 12.8733 15.5967 12.3775 15.5342 12.2742Z"
-                  fill="white"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_92_71">
-                  <rect
-                    width="20"
-                    height="20"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-[20px] lg:w-[30px]"
+                viewBox="0 0 21 20"
+                fill="none"
+              >
+                <g id="iconmonstr-whatsapp-1 1" clip-path="url(#clip0_92_71)">
+                  <path
+                    id="Vector"
+                    d="M0.547485 20L1.95332 14.8642C1.08582 13.3608 0.629985 11.6567 0.630819 9.90917C0.633319 4.44583 5.07915 0 10.5417 0C13.1925 0.000833333 15.6808 1.03333 17.5525 2.90667C19.4233 4.78 20.4533 7.27 20.4525 9.91833C20.45 15.3825 16.0042 19.8283 10.5417 19.8283C8.88332 19.8275 7.24915 19.4117 5.80165 18.6217L0.547485 20ZM6.04498 16.8275C7.44165 17.6567 8.77498 18.1533 10.5383 18.1542C15.0783 18.1542 18.7767 14.4592 18.7792 9.91667C18.7808 5.365 15.1 1.675 10.545 1.67333C6.00165 1.67333 2.30582 5.36833 2.30415 9.91C2.30332 11.7642 2.84665 13.1525 3.75915 14.605L2.92665 17.645L6.04498 16.8275ZM15.5342 12.2742C15.4725 12.1708 15.3075 12.1092 15.0592 11.985C14.8117 11.8608 13.5942 11.2617 13.3667 11.1792C13.14 11.0967 12.975 11.055 12.8092 11.3033C12.6442 11.5508 12.1692 12.1092 12.025 12.2742C11.8808 12.4392 11.7358 12.46 11.4883 12.3358C11.2408 12.2117 10.4425 11.9508 9.49665 11.1067C8.76082 10.45 8.26332 9.63917 8.11915 9.39083C7.97498 9.14333 8.10415 9.00917 8.22748 8.88583C8.33915 8.775 8.47499 8.59667 8.59915 8.45167C8.72499 8.30833 8.76582 8.205 8.84915 8.03917C8.93165 7.87417 8.89082 7.72917 8.82832 7.605C8.76582 7.48167 8.27082 6.2625 8.06499 5.76667C7.86332 5.28417 7.65915 5.34917 7.50749 5.34167L7.03249 5.33333C6.86749 5.33333 6.59915 5.395 6.37249 5.64333C6.14582 5.89167 5.50582 6.49 5.50582 7.70917C5.50582 8.92833 6.39332 10.1058 6.51665 10.2708C6.64082 10.4358 8.26248 12.9375 10.7467 14.01C11.3375 14.265 11.7992 14.4175 12.1583 14.5317C12.7517 14.72 13.2917 14.6933 13.7183 14.63C14.1942 14.5592 15.1833 14.0308 15.39 13.4525C15.5967 12.8733 15.5967 12.3775 15.5342 12.2742Z"
                     fill="white"
-                    transform="translate(0.5)"
                   />
-                </clipPath>
-              </defs>
-            </svg>
-            <svg
-              className={`w-[20px] lg:w-[30px]`}
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+                </g>
+                <defs>
+                  <clipPath id="clip0_92_71">
+                    <rect
+                      width="20"
+                      height="20"
+                      fill="white"
+                      transform="translate(0.5)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </a>
+            <a
+              href="https://www.instagram.com/juw_arquitetura/"
+              target="_blank"
+              rel="noopener"
             >
-              <g id="iconmonstr-instagram-11 1" clip-path="url(#clip0_185_74)">
-                <path
-                  id="Vector"
-                  d="M10 1.8025C12.67 1.8025 12.9867 1.8125 14.0417 1.86083C16.7517 1.98417 18.0175 3.27 18.1408 5.96C18.1892 7.01417 18.1983 7.33083 18.1983 10.0008C18.1983 12.6717 18.1883 12.9875 18.1408 14.0417C18.0167 16.7292 16.7542 18.0175 14.0417 18.1408C12.9867 18.1892 12.6717 18.1992 10 18.1992C7.33 18.1992 7.01333 18.1892 5.95917 18.1408C3.2425 18.0167 1.98333 16.725 1.86 14.0408C1.81167 12.9867 1.80167 12.6708 1.80167 10C1.80167 7.33 1.8125 7.01417 1.86 5.95917C1.98417 3.27 3.24667 1.98333 5.95917 1.86C7.01417 1.8125 7.33 1.8025 10 1.8025ZM10 0C7.28417 0 6.94417 0.0116667 5.8775 0.06C2.24583 0.226667 0.2275 2.24167 0.0608333 5.87667C0.0116667 6.94417 0 7.28417 0 10C0 12.7158 0.0116667 13.0567 0.06 14.1233C0.226667 17.755 2.24167 19.7733 5.87667 19.94C6.94417 19.9883 7.28417 20 10 20C12.7158 20 13.0567 19.9883 14.1233 19.94C17.7517 19.7733 19.775 17.7583 19.9392 14.1233C19.9883 13.0567 20 12.7158 20 10C20 7.28417 19.9883 6.94417 19.94 5.8775C19.7767 2.24917 17.7592 0.2275 14.1242 0.0608333C13.0567 0.0116667 12.7158 0 10 0ZM10 4.865C7.16417 4.865 4.865 7.16417 4.865 10C4.865 12.8358 7.16417 15.1358 10 15.1358C12.8358 15.1358 15.135 12.8367 15.135 10C15.135 7.16417 12.8358 4.865 10 4.865ZM10 13.3333C8.15917 13.3333 6.66667 11.8417 6.66667 10C6.66667 8.15917 8.15917 6.66667 10 6.66667C11.8408 6.66667 13.3333 8.15917 13.3333 10C13.3333 11.8417 11.8408 13.3333 10 13.3333ZM15.3383 3.4625C14.675 3.4625 14.1375 4 14.1375 4.6625C14.1375 5.325 14.675 5.8625 15.3383 5.8625C16.0008 5.8625 16.5375 5.325 16.5375 4.6625C16.5375 4 16.0008 3.4625 15.3383 3.4625Z"
-                  fill="#F4EFE1"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_185_74">
-                  <rect width="20" height="20" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
+              <svg
+                className="w-[20px] lg:w-[30px]"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g
+                  id="iconmonstr-instagram-11 1"
+                  clip-path="url(#clip0_185_74)"
+                >
+                  <path
+                    id="Vector"
+                    d="M10 1.8025C12.67 1.8025 12.9867 1.8125 14.0417 1.86083C16.7517 1.98417 18.0175 3.27 18.1408 5.96C18.1892 7.01417 18.1983 7.33083 18.1983 10.0008C18.1983 12.6717 18.1883 12.9875 18.1408 14.0417C18.0167 16.7292 16.7542 18.0175 14.0417 18.1408C12.9867 18.1892 12.6717 18.1992 10 18.1992C7.33 18.1992 7.01333 18.1892 5.95917 18.1408C3.2425 18.0167 1.98333 16.725 1.86 14.0408C1.81167 12.9867 1.80167 12.6708 1.80167 10C1.80167 7.33 1.8125 7.01417 1.86 5.95917C1.98417 3.27 3.24667 1.98333 5.95917 1.86C7.01417 1.8125 7.33 1.8025 10 1.8025ZM10 0C7.28417 0 6.94417 0.0116667 5.8775 0.06C2.24583 0.226667 0.2275 2.24167 0.0608333 5.87667C0.0116667 6.94417 0 7.28417 0 10C0 12.7158 0.0116667 13.0567 0.06 14.1233C0.226667 17.755 2.24167 19.7733 5.87667 19.94C6.94417 19.9883 7.28417 20 10 20C12.7158 20 13.0567 19.9883 14.1233 19.94C17.7517 19.7733 19.775 17.7583 19.9392 14.1233C19.9883 13.0567 20 12.7158 20 10C20 7.28417 19.9883 6.94417 19.94 5.8775C19.7767 2.24917 17.7592 0.2275 14.1242 0.0608333C13.0567 0.0116667 12.7158 0 10 0ZM10 4.865C7.16417 4.865 4.865 7.16417 4.865 10C4.865 12.8358 7.16417 15.1358 10 15.1358C12.8358 15.1358 15.135 12.8367 15.135 10C15.135 7.16417 12.8358 4.865 10 4.865ZM10 13.3333C8.15917 13.3333 6.66667 11.8417 6.66667 10C6.66667 8.15917 8.15917 6.66667 10 6.66667C11.8408 6.66667 13.3333 8.15917 13.3333 10C13.3333 11.8417 11.8408 13.3333 10 13.3333ZM15.3383 3.4625C14.675 3.4625 14.1375 4 14.1375 4.6625C14.1375 5.325 14.675 5.8625 15.3383 5.8625C16.0008 5.8625 16.5375 5.325 16.5375 4.6625C16.5375 4 16.0008 3.4625 15.3383 3.4625Z"
+                    fill="#F4EFE1"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_185_74">
+                    <rect width="20" height="20" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </a>
           </div>
           <div className="text-sm text-white flex flex-col lg:text-lg">
             <span>Rua das Pescadas, 75</span>
